e3fcd39ff566a0bd5bd287518a97decbb65192a2
maryalice00 <115509573+maryalice00@users.noreply.github.com>
2024-01-18 16:44:53 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import Madlib from './Madlib/madlibs';
+
+const App = () => {
+  return (
+    <div>
+      {/* Other components or content */}
+      <Madlib />
+    </div>
+  );
+}
+
+export default App;
